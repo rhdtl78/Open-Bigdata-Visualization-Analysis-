b711cede9344be25ff8399ccf4f9a00ba43e1ae2
공정훈 <rhdtl78@gmail.com>
2018-11-08 23:17:02 +0900

    패키지관리 스크립트 추가.

--- apiserver/server/views.py
+++ apiserver/server/views.py
@@ -13,7 +13,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 cred = credentials.Certificate(
-    '/home/kether/Git/obva/server/serviceAccount.json')
+    '/home/kether/Git/Open-Bigdata-Visualization-Analysis-/apiserver/server/serviceAccount.json')
 default_app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://obva1234.firebaseio.com/'
 })
