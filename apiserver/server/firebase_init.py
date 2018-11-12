b202942f9166a10d181429c4b5eb2127409e006f
공정훈 <rhdtl78@gmail.com>
2018-11-12 19:47:55 +0900

    파일 경로 문제 해결
    BASE_DIR사용해 경로 문제 해결

--- apiserver/server/firebase_init.py
+++ apiserver/server/firebase_init.py
@@ -1,9 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, db
+from django.conf import settings
+import os
 
-creditPath = ()
-cred = credentials.Certificate(
-    'apiserver/server/serviceAccount.json')
+creditPath =os.path.join(settings.BASE_DIR, 'server', 'serviceAccount.json')
+cred = credentials.Certificate(creditPath)
 default_app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://obva1234.firebaseio.com/'
 })
