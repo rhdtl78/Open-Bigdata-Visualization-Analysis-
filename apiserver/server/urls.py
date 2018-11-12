d551f0dc6a17298e3206aa17ef59c266daf78751
DongDongKim <cjdtkddkrrl@daum.net>
2018-11-12 21:30:07 +0900

    tramsform test

--- apiserver/server/urls.py
+++ apiserver/server/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url, include
 # from router.processor import outlier
 from . import views
-from .router.processor import outlier, notavailable, removeColumns
+from .router.processor import outlier, notavailable, removeColumns, transform
 from .router.analyzer import a
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^outlier$', outlier.process, name='outlier'),
     url(r'^notavailable$', notavailable.process, name='notavailable'),
-    url(r'^removeColumns$', removeColumns.process, name='removeColumns')
+    url(r'^removeColumns$', removeColumns.process, name='removeColumns'),
+    url(r'^transform$', transform.process, name='transform')
 ]
