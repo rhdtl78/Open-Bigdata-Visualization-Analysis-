4e9f2fa209fe13c6d8ee33ff2dd33c4a4377a522
DongDongKim <cjdtkddkrrl@daum.net>
2018-11-24 22:31:23 +0900

    11/24 test'

--- apiserver/server/urls.py
+++ apiserver/server/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls import url, include
 # from router.processor import outlier
 from . import views
 from .router.processor import outlier, notavailable, removeColumns, transform
-from .router.analyzer import a
+from .router.analyzer import a,regression, apriori, anova
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^outlier$', outlier.process, name='outlier'),
     url(r'^notavailable$', notavailable.process, name='notavailable'),
     url(r'^removeColumns$', removeColumns.process, name='removeColumns'),
-    url(r'^transform$', transform.process, name='transform')
+    url(r'^transform$', transform.process, name='transform'),
+    url(r'^regression$', regression.process, name='regression'),
+    url(r'^apriori$', apriori.process, name='apriori'),
+    url(r'^anova$', anova.process, name='anova')
 ]
