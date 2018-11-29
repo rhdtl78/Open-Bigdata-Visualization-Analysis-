2b8b7c5f39be16ae9ddff6098b2e81f0df92d51c
공정훈 <rhdtl78@gmail.com>
2018-11-29 18:45:51 +0900

    코드 스타일 변경

--- webserver/public/js/derModal.js
+++ webserver/public/js/derModal.js
@@ -1,61 +1,70 @@
 function derTable() {
-	$('#derBtn').empty();
+	$("#derBtn").empty();
 	var variable = new Array();
-	$('#summaryTable tr').each(function() {
-		variable.push($(this).find("td:first").text())
-	})
-
-	variable = variable.slice(1, variable.length)
-	variable.forEach(function(element) {
-		var btn = $('<button type="button" id=' + element + ' class="btn btn-primary" onclick="btnDer(this.id)">' + element + '</button>');
-		$('#derBtn').append(btn);
+	$("#summaryTable tr").each(function() {
+		variable.push(
+			$(this)
+			.find("td:first")
+			.text()
+		);
 	});
 
+	variable = variable.slice(1, variable.length);
+	variable.forEach(function(element) {
+		var btn = $(
+			'<button type="button" id=' +
+			element +
+			' class="btn btn-primary" onclick="btnDer(this.id)">' +
+			element +
+			"</button>"
+		);
+		$("#derBtn").append(btn);
+	});
 }
 
 function btnOper(oper) {
 	if (oper != "<") {
-		var temp = $('#expression').val() + oper;
-		$('#expression').val(temp);
+		var temp = $("#expression").val() + oper;
+		$("#expression").val(temp);
 	} else {
-		var temp = $('#expression').val().slice(0, -1);;
-		$('#expression').val(temp);
+		var temp = $("#expression")
+			.val()
+			.slice(0, -1);
+		$("#expression").val(temp);
 	}
-
 }
 
 function btnDer(variable) {
-	var temp = $('#expression').val() + "`" + variable + "`";
-	$('#expression').val(temp);
+	var temp = $("#expression").val() + "`" + variable + "`";
+	$("#expression").val(temp);
 }
 
 function btnDerApply() {
-	var expression = $('#expression').val();
-	var derName = $('#derName').val();
+	var expression = $("#expression").val();
+	var derName = $("#derName").val();
 
-	$('#derModal').modal('hide');
+	$("#derModal").modal("hide");
 
 	var currentUser = firebase.auth().currentUser;
 	var uid = currentUser.uid;
 
 	$.ajax({
 		data: {
-			"expression": expression,
-			"derName": derName,
-			"uid": uid
+			expression: expression,
+			derName: derName,
+			uid: uid
 		},
 		url: "/derived",
 		beforeSend: function() {
 			loading();
 		},
 		complete: function() {
-			complete()
+			complete();
 		},
 		success: function(res) {
 			var data = res.data;
 			showSummary(data);
 			showData(res.data2, res.variable);
-
 		},
 		error: function(res) {
 			// console.log(res);
